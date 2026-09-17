4f768714a85006e990572b5be6733282826d46fe
yasunuru100 <yasunuru100@gmail.com>
2026-09-17 10:49:03 +0000

    Fix Dexie DB version and manifest syntax

--- resources/js/services/db.js
+++ resources/js/services/db.js
@@ -2,7 +2,7 @@ import Dexie from 'dexie';
 
 const db = new Dexie('yuminventory-pos-db');
 
-db.version(1).stores({
+db.version(2).stores({
     products: 'id, name, sku, updated_at',
     sales: 'local_id, server_id, receipt_number, status, sync_status, created_at, sale_date',
     settings: 'key',
